b0d2cbd62b34f9bbc9606eee320ddd5699568a8d
Ryan Mottola <Rmottola68@gmail.com>
2023-06-30 17:19:16 -0400

    added contact forms to services and contact

--- src/routes/Service.jsx
+++ src/routes/Service.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import { useEffect, useState } from "react";
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
@@ -6,15 +6,29 @@ import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image';
 import { useLoaderData, useParams, Link, Outlet, useRouteLoaderData } from "react-router-dom";
 import Modal from 'react-bootstrap/Modal';
+import Alert from 'react-bootstrap/Alert';
 import Accordion from 'react-bootstrap/Accordion';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import emailjs from '@emailjs/browser';
 
 import { XCircleFill } from "react-bootstrap-icons";
 
 
 export default function Service() {
 
-
+  const form = useRef();
+  const [showAlert, setShowAlert] = useState(false);
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(`${import.meta.env.VITE_SERVICE}`, `${import.meta.env.VITE_TEMPLATE}`, form.current, `${import.meta.env.VITE_PUBLIC}`)
+      .then((result) => {
+          e.target.reset();
+          setShowAlert(true)
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   // const [show, setShow] = useState(false);
   // const handleClose = () => setShow(false);
   // const handleShow = () => setShow(true);
@@ -27,6 +41,9 @@ export default function Service() {
     setShowImageModal(id);
     setFullscreen(breakpoint);
   }
+  const [showFormModal, setShowFormModal] = useState(undefined)
+  const handleCloseFormModal = (id) => setShowFormModal(undefined);
+  const handleShowFormModal = (id) => setShowFormModal(id);
 
   const { service } = useParams()
   // console.log(service)
@@ -62,7 +79,7 @@ export default function Service() {
   // }
 
   return (
-    <Container className="my-5 d-flex justify-content-center border ">
+    <Container className="my-5 d-flex justify-content-center border">
       <Row className="text-center">
         <Col xs={12}><h1 className="fw-bold">{service}</h1></Col>
 
@@ -88,10 +105,75 @@ export default function Service() {
                   <Image src={image.guid.rendered} key={image.id} className="" onClick={() => handleCloseImageModal()} style={{ maxHeight: "850px", width: "100%", marginTop:"50px", objectFit:"contain"}} />
                   
                 </Modal>
+
+
                       
               </Col>
+              
           )}
         })}
+        <Col>                
+          <Button variant="primary" onClick={handleShowFormModal} size="lg">
+            Contact Us!
+          </Button>
+
+          <Modal show={showFormModal} onHide={handleCloseFormModal} centered>
+            <Modal.Header closeButton className="bg-white">
+              <Modal.Title>Contact Us</Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="bg-white text-center" >
+            { showAlert ?                                 
+                <Alert variant="success" onClose={() => setShowAlert(false)} dismissible className="text-center">
+                    <Alert.Heading>Thank You!</Alert.Heading>
+                    <p>
+                    Your contact information and request have been submitted to the Pavlik Photo & Design team!
+                    They'll get back to you as soon as possible!
+                    </p>
+                </Alert>
+                
+              : <div></div>
+            }
+              <form ref={form} onSubmit={sendEmail} >
+                <label className="m-2">Your Name</label>
+                <br></br>
+                <input type="text" name="from_name" required/>
+                <br></br>
+                <label className="m-2">Your Email</label>
+                <br></br>
+                <input type="email" name="user_email" required/>
+                <br></br>
+                <label className="m-2">Tell us how we can help!</label>
+                <br></br>
+                <textarea name="message" style={{width:"350px", height: "250px"}} required/>
+                <br></br>
+                <Button type="submit" value="Send" size="lg">Submit</Button>
+              </form>
+                {/* <Form className="bg-white">
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                      We'll never share your email with anyone else.
+                    </Form.Text>
+                  </Form.Group>
+
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                  </Form.Group>
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+                </Form> */}
+            </Modal.Body>
+            <Modal.Footer className="bg-white" >
+
+            </Modal.Footer>
+          </Modal>
+        </Col>
         
       </Row>
 
--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, {useRef} from "react";
+import { useEffect, useState } from "react";
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Modal from 'react-bootstrap/Modal';
 import { Telephone, Envelope } from "react-bootstrap-icons";
-40.7554936, -73.641846
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import emailjs from '@emailjs/browser';
+
 export default function Contact(){
+    const form = useRef();
+    const [showAlert, setShowAlert] = useState(false);
+    const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(`${import.meta.env.VITE_SERVICE}`, `${import.meta.env.VITE_TEMPLATE}`, form.current, `${import.meta.env.VITE_PUBLIC}`)
+        .then((result) => {
+            e.target.reset();
+            setShowAlert(true)
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+
+    const [showFormModal, setShowFormModal] = useState(undefined)
+    const handleCloseFormModal = (id) => setShowFormModal(undefined);
+    const handleShowFormModal = (id) => setShowFormModal(id);
+
     return(      
             <Container className="my-5 justify-content-center">
                 <Row className="text-center m-3">
@@ -14,6 +37,69 @@ export default function Contact(){
                         <br />
                         <Envelope color="black" size={35} className="mx-1" />Email: Andrew@pavlikphotoanddesign.com
                     </Col>
+                    <Col xs={12}>                
+                        <Button variant="primary" onClick={handleShowFormModal} size="lg">
+                            Contact Us!
+                        </Button>
+
+                        <Modal show={showFormModal} onHide={handleCloseFormModal} centered>
+                            <Modal.Header closeButton className="bg-white">
+                            <Modal.Title>Contact Us</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body className="bg-white text-center" >
+                            { showAlert ? 
+                                
+                                <Alert variant="success" onClose={() => setShowAlert(false)} dismissible className="text-center">
+                                    <Alert.Heading>Thank You!</Alert.Heading>
+                                    <p>
+                                    Your contact information and request have been submitted to the Pavlik Photo & Design team!
+                                    They'll get back to you as soon as possible!
+                                    </p>
+                                </Alert>
+                                
+                            : <div></div>
+                            }
+                            <form ref={form} onSubmit={sendEmail} >
+                                <label className="m-2">Your Name</label>
+                                <br></br>
+                                <input type="text" name="from_name" required/>
+                                <br></br>
+                                <label className="m-2">Your Email</label>
+                                <br></br>
+                                <input type="email" name="user_email" required/>
+                                <br></br>
+                                <label className="m-2">Tell us how we can help!</label>
+                                <br></br>
+                                <textarea name="message" style={{width:"350px", height: "250px"}} required/>
+                                <br></br>
+                                <Button type="submit" value="Send" >Submit</Button>
+                            </form>
+                                {/* <Form className="bg-white">
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control type="email" placeholder="Enter email" />
+                                    <Form.Text className="text-muted">
+                                    We'll never share your email with anyone else.
+                                    </Form.Text>
+                                </Form.Group>
+
+                                <Form.Group className="mb-3" controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                    <Form.Check type="checkbox" label="Check me out" />
+                                </Form.Group>
+                                <Button variant="primary" type="submit">
+                                    Submit
+                                </Button>
+                                </Form> */}
+                            </Modal.Body>
+                            <Modal.Footer className="bg-white" >
+
+                            </Modal.Footer>
+                        </Modal>
+                        </Col>
                     <Col>
                         <div id={"map-container-google-1"} className={"d-flex z-depth-1-half map-container justify-content-center py-5"} >
                             <iframe title="PAVMAP" width="1000" height="600" loading="lazy" 
